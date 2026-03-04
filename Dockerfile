d49959e9468897815d2ac87376ecacedba5d6342
Steadytrading <steadytradinginc@protonmail.com>
2026-03-04 07:23:45 +0100

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,1 @@
-
-FROM python:3.10-slim
-WORKDIR /app
-COPY . /app
-RUN pip install -r requirements.txt
-CMD ["python", "teamsteady_bot_enhanced.py"]
+force-docker-build
--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-
-#!/bin/bash
-while true; do
-  python teamsteady_bot_enhanced.py
-  echo "Restarting in 5s..."
-  sleep 5
-done
+python-telegram-bot==13.15
+psycopg2-binary
